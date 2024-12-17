0d4a94a519599bccab54f7825d480a1eba4be914
taniabarkovskya <taniabarkovskya@gmail.com>
2024-12-17 14:05:46 +0200

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { ErrorNotification } from './components/ErrorNotification';
 import { Filters } from './types/Filters';
 import { TodoList } from './components/TodoList';
 import { Footer } from './components/Footer';
+import { Header } from './components/Header';
 
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[] | null>(null);
@@ -49,24 +50,7 @@ export const App: React.FC = () => {
       <h1 className="todoapp__title">todos</h1>
 
       <div className="todoapp__content">
-        <header className="todoapp__header">
-          {/* this button should have `active` class only if all todos are completed */}
-          <button
-            type="button"
-            className="todoapp__toggle-all active"
-            data-cy="ToggleAllButton"
-          />
-
-          {/* Add a todo on form submit */}
-          <form>
-            <input
-              data-cy="NewTodoField"
-              type="text"
-              className="todoapp__new-todo"
-              placeholder="What needs to be done?"
-            />
-          </form>
-        </header>
+        <Header />
         {/* Hide the footer if there are no todos */}
 
         {todos?.length && (
